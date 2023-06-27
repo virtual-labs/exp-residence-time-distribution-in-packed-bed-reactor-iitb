6e9cd30a4d97314149b2e3141b780baa80e6413f
vleadadmin <admin@vlabs.ac.in>
2023-06-27 05:10:32 +0000

    https://virtual-labs.github.io/virtual-labs/exp-residence-time-distribution-in-packed-bed-reactor-iitb click on the link to test your code.

---
+++ simulation/js/loaded_images.js
@@ -0,0 +1,16 @@
+// all images 
+var tubular_reactor = new Image();
+tubular_reactor.src = `./images/WEBP/TubularReactor.webp`;
+var beaker = new Image();
+beaker.src = `./images/WEBP/Beaker.webp`;
+var conductivity_meter = new Image();
+conductivity_meter.src = `./images/WEBP/ConductivityMeter.webp`;
+var rotameter = new Image();
+rotameter.src = `./images/WEBP/Rotameter.webp`;
+var tracer_injector = new Image();
+tracer_injector.src = `./images/WEBP/TracerInjector.webp`;
+var centrifugal_pump = new Image();
+centrifugal_pump.src = `./images/WEBP/CentrifugalPump.webp`;
+var storage_tank = new Image();
+storage_tank.src = `./images/WEBP/StorageTank.webp`;
+//# sourceMappingURL=loaded_images.js.map
