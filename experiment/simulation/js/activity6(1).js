c8c075ace1aa5e96f94a1ee1537dee79688cb572
jaimathur <tech1@vlabsdev.in>
2023-08-11 15:54:53 +0530

    simulation code updated

---
+++ experiment/simulation/js/activity6(1).js
@@ -0,0 +1,97 @@
+var label = [];
+var data = [];
+var data1 = [];
+function activity6() {
+    pp.clearleftpannel();
+    draw_chart();
+}
+function draw_chart() {
+    document.getElementById('hide_panel3').click();
+    pp.clearleftpannel();
+    pp.addcanvas('myChart');
+    for (let i = 0; i < obt_1_data.length; i++) {
+        label.push(obt_1_data[i][0]);
+        data.push(obt_1_data[i][6]);
+        data1.push(obt_2_data[i][6]);
+    }
+    // calculate_y_datapoints();
+    var ctx = document.getElementById('myChart');
+    ctx.style.backgroundColor = "white";
+    ctx.style.marginTop = "5px";
+    ctx.style.marginLeft = "10%";
+    ctx.style.padding = "10px";
+    ctx.style.borderRadius = "8px";
+    if (typeof chart != 'undefined') {
+        chart.destroy();
+    }
+    // let labels = [0.004, 0.007, 0.010, 0.014, 0.020, 0.029, 0.039];
+    // let data1=[82.28,96.86,104.07,108.28,112.48,117.68,125.35];//hi_expt
+    // let data2=[146.90,183.50,204.11,230.09,256.89,290.83,323.49];//hi_st
+    var chart = new Chart(ctx, {
+        type: 'scatter',
+        data: {
+            labels: label,
+            datasets: [
+                {
+                    label: 'Flow rate = 200 cm^3/min',
+                    data: data,
+                    fill: false,
+                    borderColor: 'blue',
+                    tension: 0.5,
+                    showLine: true,
+                    // yAxisID: 'A',
+                    // borderWidth: 1,
+                    // borderColor: "green",
+                    // backgroundColor: "rgba(34, 139, 34, 0.5)",
+                },
+                {
+                    label: 'Flow rate = 400 cm^3/min',
+                    data: data1,
+                    fill: false,
+                    borderColor: 'red',
+                    tension: 0.5,
+                    showLine: true
+                    // yAxisID: 'A',
+                    // borderWidth: 1,
+                    // borderColor: "red",
+                    // backgroundColor: "rgba(255, 0, 0, 0.5)",
+                },
+            ]
+        },
+        options: {
+            maintainAspectRatio: true,
+            scales: {
+                y: {
+                    title: {
+                        display: true,
+                        text: 'E(t)',
+                        font: { size: 14, weight: 'bold' }
+                    }
+                },
+                x: {
+                    title: {
+                        display: true,
+                        text: 'Time (seconds)',
+                        font: { size: 14, weight: 'bold' }
+                    }
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: `Time vs E(t)`,
+                    font: { size: 18 },
+                },
+                legend: { labels: { font: { size: 14, weight: 'bold' } } }
+            },
+        }
+    });
+}
+//   function calculate_y_datapoints() {
+//     pol = regression_linear(label, data);
+//     console.log(pol);
+//     for(let i=0; i<label.length; i++) {
+//       data1.push((label[i]*pol[0]) + pol[1]);
+//     }
+//   }
+//# sourceMappingURL=activity6.js.map
